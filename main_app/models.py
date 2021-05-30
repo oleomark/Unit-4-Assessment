bc93e56d1d5e23e012628d5025aab0ca83684964
Mark Oleo <oleo.mark@gmail.com>
2021-05-30 15:43:07 -0700

    Create views,url,model,tempaltes

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+  from django.db import models
 
 # Create your models here.
+class Item(models.Model): 
+    description = models.TextField(max_length=100)
+    def __str__(self): 
+        return self.description
